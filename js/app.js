43375ce10ec46cd424627f062f3d62cfbb92e3a8
Kristoffer Velázquez Martínez <55765715+kristoffervelazquez@users.noreply.github.com>
2022-03-02 02:31:40 -0700

    v1.1
    New update: If you add an article to your shopping car, and reload the page stills in the car until you remove it.

--- js/app.js
+++ js/app.js
@@ -1,116 +1,118 @@
 // Variables
-const carrito = document.querySelector('#carrito');
-const contenedorCarrito = document.querySelector('#lista-carrito tbody');
-const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
-const listaCursos = document.querySelector('#lista-cursos');
-let articulosCarrito = [];
+const formulario = document.querySelector('#formulario');
+const listaTareas = document.querySelector('#lista-tweets');
+let tareas = [];
 
 
-cargarEventListeners();
 
-function cargarEventListeners() {
-    // Agregas un curso cuando presionas "Agrega al carrito"
-    listaCursos.addEventListener('click', agregarCurso)
-    // Eliminas un curso cuando presionas X
-    carrito.addEventListener('click', eliminarCurso)
-    // Boton de vaciar carrito
-    vaciarCarritoBtn.addEventListener('click', () => {
-        limpiarHTML();
-        articulosCarrito = [];
-    });
+// Event listeners
+eventListeners();
 
-}
+function eventListeners(){
+    // Cuando el usuario agrega un nuevo tweet
+    formulario.addEventListener('submit', agregarTarea);
 
-
-// Funciones 
-function agregarCurso(evt) {
-    evt.preventDefault();
-
-    if (evt.target.classList.contains('agregar-carrito')) {
-        const cursoSeleccionado = evt.target.parentElement.parentElement;
-        leeDatosCurso(cursoSeleccionado);
-    }
-
-}
-
-function eliminarCurso(evt){
-    if(evt.target.classList.contains('borrar-curso')){
-        const cursoId = evt.target.getAttribute('data-id');
-
-        // Elimina del arreglo de articulosCarrito por el data-id
-        articulosCarrito = articulosCarrito.filter(curso => curso.id !== cursoId)
-        carritoHTML(); // Iterar sobre el carrito y mostrar su HTML
-    }
-}
-
-
-// Lee el contenido del HTML al que le dimos click y extrae la información del curso
-function leeDatosCurso(curso) {
-    // Crear un objeto con el contenido del curso actual
-    const infoCurso = {
-        imagen: curso.querySelector('img').src,
-        titulo: curso.querySelector('h4').textContent,
-        precio: curso.querySelector('.precio span').textContent,
-        id: curso.querySelector('a').getAttribute('data-id'),
-        cantidad: 1
-    }
-
-    // Comprobar si un elemento ya existe en el carrito 
-    const existe = articulosCarrito.some(curso => curso.id === infoCurso.id);
-    console.log(existe);
-
-    if (existe) {
-        // Actualizamos cantidad
-        const cursos = articulosCarrito.map(curso => {
-            if(curso.id === infoCurso.id){
-                curso.cantidad++
-                return curso; // Retorna el objeto actualizado
-            }else{
-                return curso; // Retorna los objetos que no son actualizados
-            }
-        });
-        articulosCarrito = [...cursos];
-    } else {
-        // Agrega elementos al carrito
-        articulosCarrito = [...articulosCarrito, infoCurso];
-        // console.log(articulosCarrito)
-    }
-
-
-    carritoHTML();
-}
-
-
-// Muestra el carrito de compras en HTML
-
-function carritoHTML() {
-    // Limpiar el HTML
-    limpiarHTML();
-
-    // Recorre el carrito y agrega el HTML
-    articulosCarrito.forEach(curso => {
-        const { imagen, titulo, cantidad, id, precio } = curso;
-        const row = document.createElement('tr');
-        row.innerHTML = `
-            <td><img src="${imagen}" width=100></td>
-            <td>${titulo}</td>
-            <td>${cantidad} </td>
-            <td>${precio}</td>
-            <td>
-                <a href="#" class = "borrar-curso" data-id="${id}"> X </a>
-            </td>
-        `;
-        //Agrega el html del carrito en el body
-        contenedorCarrito.appendChild(row);
+    // Cuando el documento está listo
+    document.addEventListener('DOMContentLoaded', ()=>{
+        tareas = JSON.parse(localStorage.getItem('tareas')) || [];
+        crearHTML();
     })
 }
 
-// Elimina los cursos del tbody
-function limpiarHTML() {
-    // Forma lenta
-    // contenedorCarrito.innerHTML = '';
-    while (contenedorCarrito.firstChild) {
-        contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+
+
+//Funciones
+function agregarTarea(evt){
+    evt.preventDefault();
+    // Text area donde el usuario escribe
+    const tarea = document.querySelector('#tweet').value;
+
+    // Validacion...
+    if(tarea === ''){
+        mostrarError('La tarea no puede ir vacia');
+        return;
     }
-    
+
+    // Generar un objeto para cada tarea
+    const tareaObj = {
+        id: Date.now(),
+        tarea
+    }
+
+    //Añadir al arreglo de tareas
+    tareas = [ ...tareas, tareaObj];
+
+    // Una vez agregado crear el HTML
+    crearHTML();
+
+    // Reiniciar el formulario
+    formulario.reset();
 }
+
+
+
+
+// Mostrar mensaje de error
+function mostrarError(error){
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error');
+
+
+    //Insertarlo en el contenido
+
+    const contenido = document.querySelector('#contenido');
+    contenido.appendChild(mensajeError);
+
+    setTimeout(() =>{
+        mensajeError.remove();
+    }, 3000)
+}
+
+
+// Funcion que muestra el listado de las tareas en el documento
+function crearHTML(){
+    limpiarHTML();
+    if(tareas.length > 0){
+            tareas.forEach((tarea) => {
+                // Agregar un boton de eliminar
+                const btnEliminar = document.createElement('a');
+                btnEliminar.classList.add('borrar-tweet')
+                btnEliminar.innerText = 'X';
+                //  Añadir la funcion de eleminar
+                btnEliminar.onclick = () => {
+                    borrarTarea(tarea.id);
+                }
+
+
+                // Crear el HTML
+                const li = document.createElement('li');
+
+                // añadir el texto
+                li.innerText = tarea.tarea;
+
+                // Añadir el boton
+                li.appendChild(btnEliminar);
+
+                // insertarlo en el HTML
+                listaTareas.appendChild(li);
+            })
+    }
+    sincronizarStorage();
+}
+function sincronizarStorage(){
+    localStorage.setItem('tareas', JSON.stringify(tareas));
+}   
+
+// Elimina una tarea
+function borrarTarea(id){
+    tareas = tareas.filter( tarea => tarea.id !== id);
+    crearHTML();
+}
+
+function limpiarHTML(){
+    while(listaTareas.firstChild){
+        listaTareas.removeChild(listaTareas.firstChild);
+    }
+}
+
